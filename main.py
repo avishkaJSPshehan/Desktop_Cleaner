64e2dfb23b882ae3a07832a8f0204c81fe7f5d9c
Avishka Shehan <152954657+avishkaJSPshehan@users.noreply.github.com>
2024-06-01 01:30:39 +0530

    file uploding
    this is the simple code for clean the desktop and also this is CMD application. GUI version coming soon

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+path = input("Enter Folder Path : ")
+files = os.listdir(path)
+print(files)
+
+for file in files:
+    filename,extension = os.path.splitext(file)
+    extension = extension[1:]
+
+    if os.path.exists(path+'/'+extension):
+        shutil.move(path+'/'+file,path+'/'+extension+'/'+file)
+
+    else:
+        os.makedirs(path+'/'+extension)
+        shutil.move(path+'/'+file,path+'/'+extension+'/'+file)
